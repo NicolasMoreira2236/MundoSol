5985c1d1389a816afc31a669619895eceefe4792
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-17 17:33:57 -0300

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import logo from './logoestetica.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>Mundo Cosmetica Sol</p>
+        <a className="App-link" href="https://nicolasmoreira2236.github.io/EsteticaOnline/" target="_blank" rel="noopener noreferrer">
+          Ingresa aqui!
         </a>
       </header>
     </div>
   );
 }
-
 export default App;
