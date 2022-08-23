2f441ca4f67c1e2dfe9d35690843dfdc4c0979a7
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-22 22:32:11 -0300

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,20 @@
 const Navbar = () => {
     return (
-    <nav>
-        Hola mundo.
-    </nav>
+        <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="../src/logoestetica.png" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+            Bootstrap
+          </a>
+          <a href="#">Quienes somos?</a>
+          <a href="#">Productos</a>
+          <a href="#">Contacto</a>
+          </div>
+
+          </nav>
+        
     )
+
     
 }
 
