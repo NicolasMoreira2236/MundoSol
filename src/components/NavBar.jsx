1f0bb294e050be5ee8eb4f80501b4765e6ef2b43
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-24 10:43:59 -0300

    solucinado logo nav no estaba importado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,17 @@
+
+import logo from '../logoestetica.png';
+
 const Navbar = () => {
     return (
-        <nav class="navbar bg-light">
+        <nav class="navbar bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="../src/logoestetica.png" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
+          <a class="navbar-brand text-white" href="">
+            <img src={logo} alt="" width="40" class="d-inline-block align-text-center"/>
             Bootstrap
           </a>
-          <a href="#">Quienes somos?</a>
-          <a href="#">Productos</a>
-          <a href="#">Contacto</a>
+          <a href="">Quienes somos?</a>
+          <a href="">Productos</a>
+          <a href="">Contacto</a>
           </div>
 
           </nav>
