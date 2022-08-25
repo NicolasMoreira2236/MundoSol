82e7fe0bddcf40555b2d720aef961f2a964d6318
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-25 08:57:17 -0300

    mejorando la page

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,14 +1,26 @@
+import styled from 'styled-components';
 
-    const ItemListContainer = (props) => {
+const ItemListContainer = (props) => {
         const {contenido} = props
-        return ( 
+        return ( <Greet>
             <div>
             <h1 class="text-center">
             Hola Mundovich.
             </h1> 
+            
             {contenido}
             </div>
+            </Greet>
         ) 
     }
     
     export default  ItemListContainer;
+
+
+    const Greet = styled.div`
+    h1{
+    color: #cf1717;
+    background-color: black;}
+    `
+    
+
