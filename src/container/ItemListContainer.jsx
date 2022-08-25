7a308c80d2b4c6e8ae31f31068b77726a51abc78
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-25 13:42:58 -0300

    update

--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 const ItemListContainer = (props) => {
         const {contenido} = props
-        return ( <Greet>
+        return ( 
+        <Greet>
             <div>
-            <h1 class="text-center">
-            Hola Mundovich.
-            </h1> 
-            
-            {contenido}
+                <h1 class="text-center">
+                    Bienvenidos a Mundo Cosmetica.
+                </h1> 
+                {contenido}
             </div>
-            </Greet>
+        </Greet>
         ) 
     }
     
@@ -18,9 +18,11 @@ const ItemListContainer = (props) => {
 
 
     const Greet = styled.div`
-    h1{
-    color: #cf1717;
-    background-color: black;}
-    `
-    
 
+    h1{
+        color: #cf1717;
+        background-color: black;
+        text-transform: uppercase;
+        text-transform: bold;
+        margin: 0;
+    }`
