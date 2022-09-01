f35eb31a8913a60e89a93be76e13d0a6c5ec2c8d
¨Nicolas.Moreira¨ <nicolas-moreira1@hotmail.com>
2022-08-31 22:15:23 -0300

    commiteo

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -1,6 +1,21 @@
 import Item from "./item"
-import React from "react";
+import React, { useState,useEffect } from "react";
 
+const [carrito, SetCarrito] = useState([])
+const [productos, SetProductos] = useState([])
+
+const BuscarProductos = async() => {
+    try {
+        const response = await fetch('../src/ArrayItem.json')
+        const data = await response.json();
+        SetProductos(data.results);
+    }catch(e){
+    }
+}
+
+useEffect(() => { 
+    
+    BuscarProductos()}, [] )
 
 const ItemList = ({productos}) => {
     return (
